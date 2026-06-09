aedb3b8f4e2f946936749166b7137ee42f62f124
Allan-Wariss <allanwariss2003@gmail.com>
2026-06-09 19:07:38 -0300

    att

--- front/src/components/FilterChips/index.tsx
+++ front/src/components/FilterChips/index.tsx
@@ -5,7 +5,7 @@ type FilterOption = 'todos' | 'meus'
 
 const CHIPS: { label: string; value: FilterOption }[] = [
     { label: 'Todos', value: 'todos' },
-    { label: 'Meus', value: 'meus' },
+    { label: 'Meu', value: 'meus' },
 ]
 
 export const FilterChips = () => {
